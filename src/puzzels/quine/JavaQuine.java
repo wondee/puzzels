d4254dfd14ab9a4772ec38b634bc825dd92b10cf
cb2woak <cb2woak@AP551938.zitd.global.commerzbank.com>
2014-07-31 10:36:45 +0200

    added Quine challange

---
+++ src/puzzels/quine/JavaQuine.java
@@ -0,0 +1,24 @@
+package puzzels.quine;
+
+import java.io.StringWriter;
+import java.io.Writer;
+
+import com.strobel.decompiler.Decompiler;
+import com.strobel.decompiler.PlainTextOutput;
+
+public class JavaQuine {
+	
+	public static void main(String[] args) {
+		printClass(JavaQuine.class);
+	}
+	
+	private static final void printClass(Class<?> c) {
+		
+		Writer writer = new StringWriter();
+		Decompiler.decompile(c.getName().replace(".", "/"), new PlainTextOutput(writer ));
+		
+		System.out.println(writer.toString());
+		
+	}
+	
+}
